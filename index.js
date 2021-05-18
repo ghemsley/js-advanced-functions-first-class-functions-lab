f5137c76680a40d5e8be55a4ecff52a2c71d047d
ghemsley <ghemsley@protonmail.ch>
2021-05-18 16:35:47 -0500

    commit tree 757b7ee8e1d0185bb7b563957a1f85f5366096b8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2)
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (mulitplier) => (fare) => fare * mulitplier
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, func) => func(drivers)
